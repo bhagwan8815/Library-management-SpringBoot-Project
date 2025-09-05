813fbad4756af8dc88399e3dab7f94d71061666d
bhagwan8815 <ranabhagwan8815@gmail.com>
2025-09-05 22:00:06 +0530

    JWT token implement

--- library-management/src/main/java/com/rana/library_management/entity/User.java
+++ library-management/src/main/java/com/rana/library_management/entity/User.java
@@ -2,13 +2,18 @@ package com.rana.library_management.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Data
 @Table(name="users")  //hibernate will create a table with the name of users
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
     private Long id;
@@ -20,4 +25,15 @@ public class User {
                                                   // (2) here fetch = FetchType.EAGER EAGER means the user roles is also loaded when i call UserRepo.findById(id)----> it will give user with their toles. but if i put her LAZY insted of EAGER then if i try to get user roles by calling userRepo.findById(id) then user.getRoles() it will return LAZYIntitilization ERROR
     private Set<String> roles;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
 }
---
+++ library-management/src/main/java/com/rana/library_management/services/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.rana.library_management.services;
+
+import com.rana.library_management.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        return userRepository.findByUserName(username)
+                .orElseThrow(()->new RuntimeException("user not found ."));
+    }
+}
